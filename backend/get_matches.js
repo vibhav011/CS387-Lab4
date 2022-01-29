69a6e8f81dbfdfc396149681c799432b8d17344b
vibhav011 <vibhav200601@gmail.com>
2022-01-29 21:16:45 +0530

    Added environment variables

--- backend/get_matches.js
+++ backend/get_matches.js
@@ -1,13 +1,19 @@
 function get_matches(db_client, request, response) { // request and response are parts of the http get method
-var ret_json = null
-const matches_query = 'SELECT match_id, (SELECT team_name from team where team_id = team1) team1_name,\
+  var ret_json = null
+  const matches_query = 'SELECT match_id, (SELECT team_name from team where team_id = team1) team1_name,\
                      (SELECT team_name from team where team_id = team2) team2_name, venue_name, \
                      city_name, (SELECT team_name from team where team_id = match_winner) winner FROM \
                      venue, match where venue.venue_id = match.venue_id order by season_year desc \
                    offset $1 rows fetch next $2 rows only'
-db_client.query(matches_query, [request.query.skip, request.query.limit], (err, res) => {
-    console.log(err)
-    ret_json = JSON.stringify(res['rows'])
+  db_client.query(matches_query, [request.query.skip, request.query.limit], (err, res) => {
+    if (err) {
+      console.log(err)
+      response.status(500).json({ error: err.message })
+    } else {
+      ret_json = res.rows
+      response.status(200).json({ data: ret_json })
+    }
   })
-return ret_json
 }
+
+module.exports = get_matches;
--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,27 @@
 var express = require('express');
+const path = require('path')
 var app = express();
-
 app.use(express.static('public'));
 
-// creating a pool
-const { Client} = require('pg')
-const db_client = new Client({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'lab2db',
-  password: '',
-  port: 5432,
-})
+// creating a client
+require('dotenv').config({path: path.resolve(__dirname+"/.env")});
+const { Client } = require('pg')
 
-;(async () => {
-await db_client.connect()
+const db_client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PSWD,
+  port: process.env.DB_PORT,
+});
+
+(async () => {
+  await db_client.connect()
 })()
 
+const get_matches = require('./get_matches');
 app.get('/matches', function (request, response) {
-  // call get function
+  get_matches(db_client, request, response);
 })
 
 
