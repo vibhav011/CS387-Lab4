f9ec6a62b45bf92ff220c5710bab9ccc3f187c84
vibhav011 <vibhav200601@gmail.com>
2022-01-31 01:44:54 +0530

    Connected score comparison chart with backend

--- frontend/src/components/MatchInfo/index.js
+++ frontend/src/components/MatchInfo/index.js
@@ -19,13 +19,15 @@ import ScorecardElement from "./scorecard";
 import ScoreComparisonElement from "./scoreComparison";
 import Card from '@mui/material/Card';
 import Summary from "./summary";
+import { CardContent } from "@mui/material";
 
 class MatchInfo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             expanded: 'panel1',
-            b2data: {}
+            b2data: {},
+            b3data: {}
         };
     }
 
@@ -38,88 +40,105 @@ class MatchInfo extends React.Component {
 
     fetchData(id) {
         fetch(`http://localhost:8081/matches/scorecard/${id}`)
-        .then(res => res.json().then(data => ({ status: res.status, body: data })))
-        .then(data => {
-            let body = data.body;
+            .then(res => res.json().then(data => ({ status: res.status, body: data })))
+            .then(data => {
+                let body = data.body;
 
-            if (data.status === 200) {
-            console.log(body);
-            this.setState({b2data: body});
-            }
-            else {
-            console.log("Error in Matches fetch");
-            console.log(body.error);
-            }
-        });
+                if (data.status === 200) {
+                    console.log(body);
+                    this.setState({ b2data: body });
+                }
+                else {
+                    console.log("Error in scorecard fetch");
+                    console.log(body.error);
+                }
+            });
+
+        fetch(`http://localhost:8081/matches/score_comparison/${id}`)
+            .then(res => res.json().then(data => ({ status: res.status, body: data })))
+            .then(data => {
+                let body = data.body;
+
+                if (data.status === 200) {
+                    console.log(body);
+                    this.setState({ b3data: body });
+                }
+                else {
+                    console.log("Error in score_comparison fetch");
+                    console.log(body.error);
+                }
+            });
     }
 
     handleChange = (panel) => (event, isExpanded) => {
-        this.setState({expanded: isExpanded ? panel : false});
+        this.setState({ expanded: isExpanded ? panel : false });
     };
 
     render() {
-    return (
-        <BaseLayout title="Match Info"
-            breadcrumb={[
-                { label: "Home", route: "/" },
-                { label: "Match Info" },
-            ]}>
-            <MKBox component="section" py={{ xs: 3, md: 3 }}>
-                <Container>
-                    <Grid container alignItems="center" justifyContent="center">
-                        <Accordion expanded={this.state.expanded === 'panel1'} onChange={this.handleChange('panel1')} sx = {{borderRadius: '10px', width: "85%"}}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon sx ={{color:"#fff"}} />}
-                                aria-controls="panel1bh-content"
-                                id="panel1bh-header"
-                                sx = {{backgroundColor: '#384664', borderRadius: '10px'}}
-                            >
-                                <Typography sx={{ width: '33%', flexShrink: 0, color:"#fff", fontWeight:"bold" }}>
-                                    Scorecard
-                                </Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                            <ScorecardElement data={this.state.b2data}/>
-                            </AccordionDetails>
-                        </Accordion>
+        return (
+            <BaseLayout title="Match Info"
+                breadcrumb={[
+                    { label: "Home", route: "/" },
+                    { label: "Match Info" },
+                ]}>
+                <MKBox component="section" py={{ xs: 3, md: 3 }}>
+                    <Container>
+                        <Grid container alignItems="center" justifyContent="center">
+                            <Accordion expanded={this.state.expanded === 'panel1'} onChange={this.handleChange('panel1')} sx={{ borderRadius: '10px', width: "85%" }}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon sx={{ color: "#fff" }} />}
+                                    aria-controls="panel1bh-content"
+                                    id="panel1bh-header"
+                                    sx={{ backgroundColor: '#384664', borderRadius: '10px' }}
+                                >
+                                    <Typography sx={{ width: '33%', flexShrink: 0, color: "#fff", fontWeight: "bold" }}>
+                                        Scorecard
+                                    </Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <ScorecardElement data={this.state.b2data} />
+                                </AccordionDetails>
+                            </Accordion>
 
-                        <Accordion expanded={this.state.expanded === 'panel2'} onChange={this.handleChange('panel2')} sx = {{borderRadius: '10px', width: "85%", marginTop: '10px', marginBottom: '10px'}}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon sx ={{color:"#fff"}}/>}
-                                aria-controls="panel2bh-content"
-                                id="panel2bh-header"
-                                sx = {{backgroundColor: '#384664', borderRadius: '10px'}}
-                            >
-                                <Typography sx={{ width: '33%', flexShrink: 0, color:"#fff", fontWeight:"bold" }}>
-                                    Score Comparison
-                                </Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Card sx = {{marginLeft: 'auto', marginRight:'auto', padding:'5px', width:'800px', height:'400px'}}>{ScoreComparisonElement}</Card>
-                            </AccordionDetails>
-                        </Accordion>
+                            <Accordion expanded={this.state.expanded === 'panel2'} onChange={this.handleChange('panel2')} sx={{ borderRadius: '10px', width: "85%", marginTop: '10px', marginBottom: '10px' }}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon sx={{ color: "#fff" }} />}
+                                    aria-controls="panel2bh-content"
+                                    id="panel2bh-header"
+                                    sx={{ backgroundColor: '#384664', borderRadius: '10px' }}
+                                >
+                                    <Typography sx={{ width: '33%', flexShrink: 0, color: "#fff", fontWeight: "bold" }}>
+                                        Score Comparison
+                                    </Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Card sx={{ marginLeft: 'auto', marginRight: 'auto', paddingLeft: '5px', paddingTop: '20px', width: '800px', height: '425px' }}>
+                                        <ScoreComparisonElement data={this.state.b3data} />
+                                    </Card>
+                                </AccordionDetails>
+                            </Accordion>
 
-                        <Accordion expanded={this.state.expanded === 'panel3'} onChange={this.handleChange('panel3')} sx = {{borderRadius: '10px', width: "85%"}}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon sx ={{color:"#fff"}} />}
-                                aria-controls="panel3bh-content"
-                                id="panel3bh-header"
-                                sx = {{backgroundColor: '#384664', borderRadius: '10px'}}
-                            >
-                                <Typography sx={{ width: '33%', flexShrink: 0, color:"#fff", fontWeight:"bold" }}>
-                                    Summary
-                                </Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                            {Summary}
-                            </AccordionDetails>
-                        </Accordion>
-                    </Grid>
-                </Container>
-            </MKBox>
-        </BaseLayout>
-    );
-        }
+                            <Accordion expanded={this.state.expanded === 'panel3'} onChange={this.handleChange('panel3')} sx={{ borderRadius: '10px', width: "85%" }}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon sx={{ color: "#fff" }} />}
+                                    aria-controls="panel3bh-content"
+                                    id="panel3bh-header"
+                                    sx={{ backgroundColor: '#384664', borderRadius: '10px' }}
+                                >
+                                    <Typography sx={{ width: '33%', flexShrink: 0, color: "#fff", fontWeight: "bold" }}>
+                                        Summary
+                                    </Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    {Summary}
+                                </AccordionDetails>
+                            </Accordion>
+                        </Grid>
+                    </Container>
+                </MKBox>
+            </BaseLayout>
+        );
+    }
 }
 
 export default (props) => (<MatchInfo params={useParams()} />);
--- frontend/src/components/MatchInfo/scoreComparison.js
+++ frontend/src/components/MatchInfo/scoreComparison.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Dot } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Dot, Label } from 'recharts';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const data = [
     {
@@ -47,48 +48,74 @@ const data = [
 ];
 
 const CustomTooltip = ({ active, payload, label }) => {
-    // console.log(payload);
     if (active && payload && payload.length) {
-      return (
-        <div className="custom-tooltip">
-          <p className="label">{`${label} : ${payload[0].value}`}</p>
-          <p className="desc">Anything you want can be displayed here.</p>
-        </div>
-      );
+        return (
+            <div className="custom-tooltip">
+                <p className="label">{`${label} : ${payload[0].value}`}</p>
+                <p className="desc">Anything you want can be displayed here.</p>
+            </div>
+        );
     }
-  
+
     return null;
-  };
+};
 
 function renderDot(props) {
-    const { cx, cy, stroke, payload, value } = props;
-    if (value > 2500) {
+    const { cx, cy, dataKey, stroke, payload } = props;
+    let k = (dataKey === 'innings1') ? 'team1_wicket' : 'team2_wicket';
+    if (payload[k]) {
         return (
-            <Dot cx = {cx} cy = {cy} r = {3} fill = {stroke} />
+            <Dot cx={cx} cy={cy} r={5} fill={stroke} />
         )
     }
     return null;
 }
 
-const ScoreComparisonElement = <ResponsiveContainer width={770} height={385}>
-    <LineChart
-        // width={500}
-        // height={300}
-        data={data}
-        margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-        }}
-    >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        {/* <Tooltip content={<CustomTooltip />} /> */}
-        <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" dot={renderDot} />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-    </LineChart>
-</ResponsiveContainer>
+function formatter(data) {
+    let newData = [];
+
+    let sum = 0;
+    data['innings1'].map((element, i) => {
+        sum += parseInt(element['runs']);
+        newData.push({
+            name: i + 1,
+            innings1: sum,
+            team1_wicket: element['wicket']
+        });
+    });
+    sum = 0;
+    data['innings2'].map((element, i) => {
+        sum += parseInt(element['runs']);
+        newData[i]['innings2'] = sum;
+        newData[i]['team2_wicket'] = element['wicket'];
+    });
+    return newData;
+}
+
+function ScoreComparisonElement(props) {
+    // let data = props.data;
+    if (Object.keys(props.data).length === 0) {
+        return (<><CircularProgress /></>)
+    }
+
+    return (<ResponsiveContainer width={770} height={385}>
+        <LineChart
+            data={formatter(props.data)}
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}
+        >   
+            <Line name = {props.data['team1_name'][0]['team_name']} type="monotone" dataKey="innings1" stroke="#8884d8" dot={renderDot} isAnimationActive={false} />
+            <Line name = {props.data['team2_name'][0]['team_name']} type="monotone" dataKey="innings2" stroke="#82ca9d" dot={renderDot} isAnimationActive={false} />
+            <Tooltip />
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name"><Label position="bottom" value="Overs" /></XAxis>
+            <YAxis><Label position="left" angle="-90" value="Runs" /></YAxis>
+            <Legend wrapperStyle={{ top: 0, left: 25 }} />
+        </LineChart>
+    </ResponsiveContainer>)
+}
 export default ScoreComparisonElement;
