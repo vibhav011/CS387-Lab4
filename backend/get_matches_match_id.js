7b9fcf1727adc74d0f5b87c94e2a8cf1b261ac58
Tulip16 <tulip@cse.iitb.ac.in>
2022-01-30 17:32:43 +0530

    section b2 done

--- backend/get_matches_match_id.js
+++ backend/get_matches_match_id.js
@@ -1,11 +1,4 @@
-function json_concat(o1, o2) {
-  for (var key in o2) {
-   o1[key] = o2[key];
-  }
-  return o1;
- }
-
- async function updateRetJSON(db_client, query, params, ret_json, error, key) {
+async function updateRetJSON(db_client, query, params, ret_json, error, key) {
   try {
     res = await db_client.query(query, params)
     ret_json[key] = res.rows
@@ -16,7 +9,22 @@ function json_concat(o1, o2) {
   }
 }
 
-async function get_batting(db_client, match_id, innings_no, ret_json, error) { // request and response are parts of the http get method
+async function updateArrayJSON(db_client, query, params, ret_json, error, key1, key2) {
+  try {
+    res = await db_client.query(query, params)
+    if (!(key1 in ret_json)) ret_json[key1] = {}
+    ret_json[key1][key2] = []
+    for (let i=0; i < res.rows.length; i++) {
+      ret_json[key1][key2][i] = res.rows[i][key2]
+    }
+  }
+  catch (err) {
+    error.status = 500;
+    error.message = err.message;
+  }
+}
+
+async function get_batting(db_client, match_id, innings_no, ret_json, error) {
   const query = '	select player_name as batter, runs, fours, sixes, balls_faced from player, \
   (select striker, sum(runs_scored) as runs, count(sixes) as sixes, \
   count(fours) as fours, count(*) as balls_faced from \
@@ -28,91 +36,52 @@ async function get_batting(db_client, match_id, innings_no, ret_json, error) { /
   await updateRetJSON(db_client, query, [match_id, innings_no], ret_json, error, 'batting')
 }
 
-function get_bowling(db_client, match_id, innings_no) {
+async function get_bowling(db_client, match_id, innings_no, ret_json, error) {
   const query = 'select player_name as bowler, balls_bowled, runs_given, wickets \
   from player, (select bowler, count(*) as balls_bowled, sum(runs_scored) as runs_given, count(wickets) as wickets \
    from (select bowler, match_id, runs_scored, case when out_type != \'run out\' and out_type != \'retired hurt\' and out_type is not NULL then 1 end wickets\
       from ball_by_ball where match_id = $1 and innings_no = $2) aggs group by bowler) ball_info where bowler = player_id'
-  db_client.query(query, [match_id, innings_no], (err, res) => {
-    if (err) {
-      return err, null
-    } else {
-      return null, res.rows
-    }
-  })
+  
+  await updateRetJSON(db_client, query, [match_id, innings_no], ret_json, error, 'bowling')
 }
 
-function get_match_info(db_client, match_id, innings_no) {
-  var ret_json = {}
+async function get_players(db_client, match_id, ret_json, error) {
+  const query1 = 'select player_name as team1_players from player, player_match, team, match \
+  where player_match.match_id = match.match_id and player.player_id = player_match.player_id \
+  and team.team_id = player_match.team_id and team1 = team.team_id and match.match_id = $1'
+  await updateArrayJSON(db_client, query1, [match_id], ret_json, error, 'players', 'team1_players')
+
+  const query2 = 'select player_name as team2_players from player, player_match, team, match \
+  where player_match.match_id = match.match_id and player.player_id = player_match.player_id \
+  and team.team_id = player_match.team_id and team2 = team.team_id and match.match_id = $1'
+  await updateArrayJSON(db_client, query2, [match_id], ret_json, error, 'players', 'team2_players')
+}
+
+async function get_umpires(db_client, match_id, ret_json, error) {
+  const query = 'select umpire_name as umpire_names from umpire, umpire_match where \
+  umpire.umpire_id = umpire_match.umpire_id and match_id = $1'
+  await updateArrayJSON(db_client, query, [match_id], ret_json, error, 'umpires', 'umpire_names')
+}
+
+async function get_match_info(db_client, match_id, ret_json, error) {
   const query = 'select match_id, (SELECT team_name from team where team_id = team1) as team1_name, \
   (SELECT team_name from team where team_id = team2) as team2_name, season_year as year_of_playing, \
   toss_name, (SELECT team_name from team where team_id = toss_winner) as toss_winner_name, \
   (SELECT venue_name from venue where venue.venue_id = match.venue_id) as venue from match where match_id = $1'
-  db_client.query(query, [match_id], (err, res) => {
-    if (err) {
-      return err, null
-    } else {
-      json_concat(ret_json, res.rows)
-    }
-  })
 
-  const players1_query = 'select player_name as team1_players from player, player_match, team, match \
-  where player_match.match_id = match.match_id and player.player_id = player_match.player_id \
-  and team.team_id = player_match.team_id and team1 = team.team_id and match.match_id = $1'
-  db_client.query(players1_query, [match_id], (err, res) => {
-    if (err) {
-      return err, null
-    } else {
-      json_concat(ret_json, res.rows)
-    }
-  })
-
-  const players2_query = 'select player_name as team2_players from player, player_match, team, match \
-  where player_match.match_id = match.match_id and player.player_id = player_match.player_id \
-  and team.team_id = player_match.team_id and team2 = team.team_id and match.match_id = $1'
-  db_client.query(players2_query, [match_id], (err, res) => {
-    if (err) {
-      return err, null
-    } else {
-      json_concat(ret_json, res.rows)
-    }
-  })
-
-  const umpires_query = 'select umpire_name as umpires from umpire, umpire_match where \
-  umpire.umpire_id = umpire_match.umpire_id and match_id = $1'
-  db_client.query(umpires_query, [match_id], (err, res) => {
-    if (err) {
-      return err, null
-    } else {
-      json_concat(ret_json, res.rows)
-    }
-  })
-
-  return null, ret_json
+  await updateRetJSON(db_client, query, [match_id], ret_json, error, 'info')
 }
 
-function get_extras(db_client, match_id, innings_no) {
+async function get_extras(db_client, match_id, innings_no, ret_json, error) {
   const query = 'select sum(extra_runs) from ball_by_ball where match_id = $1 and innings_no = $2'
-  db_client.query(query, [match_id, innings_no], (err, res) => {
-    if (err) {
-      return err, null
-    } else {
-      return null, res.rows
-    }
-  })
+  await updateRetJSON(db_client, query, [match_id, innings_no], ret_json, error, 'extras')
 }
 
-function get_score(db_client, match_id, innings_no) {
+async function get_score(db_client, match_id, innings_no, ret_json, error) {
   const query = 'select sum(runs_scored)+sum(extra_runs) as runs, count(wickets) as wickets from (select runs_scored, extra_runs, \
   case when out_type != \'run out\' and out_type != \'retired hurt\' and out_type is not NULL then 1 end wickets \
   from ball_by_ball where match_id = $1 and innings_no = $2) ball_info'
-  db_client.query(query, [match_id, innings_no], (err, res) => {
-    if (err) {
-      return err, null
-    } else {
-      return null, res.rows
-    }
-  })
+  await updateRetJSON(db_client, query, [match_id, innings_no], ret_json, error, 'score')
 }
 
 async function get_match_id_json(db_client, match_id, ret_json, err) {
@@ -120,46 +89,23 @@ async function get_match_id_json(db_client, match_id, ret_json, err) {
     await get_batting(db_client, match_id, i, ret_json['innings'+i], err)
     if (err.status !== 200) return;
 
-  //   get_bowling(db_client, match_id, i, (err, res) => {
-  //     if(err) {
-  //       console.log('innings '+ i +'bowling', err)
-  //       response.status(500).json({ error: err.message })
-  //       is_err = true
-  //     } else {
-  //       ret_json['innings'+i]['bowling'] = res
-  //     }
-  //   })
+    await get_bowling(db_client, match_id, i, ret_json['innings'+i], err)
+    if (err.status !== 200) return;
 
-  //   get_extras(db_client, match_id, 1, (err, res) => {
-  //     if(err) {
-  //       console.log('innings '+ i +'bowling', err)
-  //       response.status(500).json({ error: err.message })
-  //       is_err = true
-  //     } else {
-  //       ret_json['innings'+i]['extras'] = res
-  //     }
-  //   })
+    await get_extras(db_client, match_id, i, ret_json['innings'+i], err)
+    if (err.status !== 200) return;
 
-  //   get_score(db_client, match_id, 1, (err, res) => {
-  //     if(err) {
-  //       console.log('innings '+ i +'bowling', err)
-  //       response.status(500).json({ error: err.message })
-  //       is_err = true
-  //     } else {
-  //       ret_json['innings'+i]['score'] = res
-  //     }
-  //   })
-  // }
+    await get_score(db_client, match_id, i, ret_json['innings'+i], err)
+    if (err.status !== 200) return;
 
-  // get_match_info(db_client, match_id, (err, res) => {
-  //   if(err) {
-  //     console.log('match info', err)
-  //     response.status(500).json({ error: err.message })
-  //     is_err = true
-  //   } else {
-  //     ret_json['match info'] = res
-  //   }
-  // })
+    await get_match_info(db_client, match_id, ret_json['match_info'], err)
+    if (err.status !== 200) return;
+
+    await get_players(db_client, match_id, ret_json['match_info'], err)
+    if (err.status !== 200) return;
+
+    await get_umpires(db_client, match_id, ret_json['match_info'], err)
+    if (err.status !== 200) return;    
   }
 }
 
