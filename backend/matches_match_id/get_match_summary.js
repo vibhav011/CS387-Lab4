6a17b2b44695a8ba68c884f544fe58ccb550e7a0
Tulip16 <tulip@cse.iitb.ac.in>
2022-01-31 12:09:59 +0530

    corrected one team match summary error

--- backend/matches_match_id/get_match_summary.js
+++ backend/matches_match_id/get_match_summary.js
@@ -9,21 +9,21 @@ async function updateRetJSON(db_client, query, params, ret_json, error, key) {
     }
 }
 
-async function get_match_summary_json(db_client, match_id, ret_json, error) {
+async function get_match_summary_json(db_client, match_id, innings_no, ret_json, error) {
     const query1 = 'SELECT player_name as batsman_name, runs, balls_faced from player, (SELECT batsman, runs, balls_faced, dense_rank() over (order by runs desc, balls_faced asc, batsman asc) batsman_rank \
     from (SELECT striker as batsman, sum(runs_scored) as runs, count(*) as balls_faced from match, ball_by_ball where \
-    ball_by_ball.match_id = match.match_id and match.match_id = $1 group by striker) sq ) sq2 where batsman_rank < 4 and balls_faced > 0 \
+    ball_by_ball.match_id = match.match_id and match.match_id = $1 and innings_no = $2 group by striker) sq ) sq2 where batsman_rank < 4 and balls_faced > 0 \
     and player.player_id = batsman order by batsman_rank'
   
-    await updateRetJSON(db_client, query1, [match_id], ret_json, error, 'batsmen')
+    await updateRetJSON(db_client, query1, [match_id, innings_no], ret_json, error, 'batsmen')
   
     const query2 = 'SELECT player_name as bowler_name,  wickets, runs_given, overs_bowled from player, (SELECT bowler, wickets, runs_given, overs_bowled, dense_rank() \
     over (order by wickets desc, runs_given asc, bowler asc) bowler_rank from \
     (SELECT bowler, count(out_type) as wickets, count(runs_scored+extra_runs) as runs_given, \
-    count(distinct over_id) as overs_bowled from match, ball_by_ball where ball_by_ball.match_id = match.match_id and match.match_id = $1 \
+    count(distinct over_id) as overs_bowled from match, ball_by_ball where ball_by_ball.match_id = match.match_id and match.match_id = $1 and innings_no = $2\
     group by bowler) sq) sq2 where bowler_rank < 4 and wickets > 0 and player.player_id = bowler order by bowler_rank'
     
-    await updateRetJSON(db_client, query2, [match_id], ret_json, error, 'bowlers')
+    await updateRetJSON(db_client, query2, [match_id, innings_no], ret_json, error, 'bowlers')
 }
 
 async function get_match_summary(db_client, request, response) { 
@@ -32,9 +32,10 @@ async function get_match_summary(db_client, request, response) {
       response.status(400).json({ error: 'match_id must be an integer' })
       return
     }
-    var ret_json = {'batsmen':{}, 'bowlers':{}}
+    var ret_json = {'team1':{'batsmen':{}, 'bowlers':{}}, 'team2':{'batsmen':{}, 'bowlers':{}}}
     var err = {status: 200, message: 'success'}
-    await get_match_summary_json(db_client, match_id, ret_json, err)
+    await get_match_summary_json(db_client, match_id, 1, ret_json['team1'], err)
+    await get_match_summary_json(db_client, match_id, 2, ret_json['team2'], err)
   
     if (err.status === 200) {
       response.status(200).json(ret_json)
