9775adfe09716ab25e5c22069d5d5f3498292d9d
Tulip16 <tulip@cse.iitb.ac.in>
2022-02-04 16:14:05 +0530

    F done

--- backend/venues/get_form.js
+++ backend/venues/get_form.js
@@ -1,16 +1,11 @@
 function get_form(db_client, request, response) { // request and response are parts of the http get method
     var ret_json = null
 
-    var capacity = parseInt(request.body.capacity);
-    if (!capacity) {
-      response.status(400).json({ error: 'capacity must be an integer' })
-      return
-    }
 
     const query = 'CREATE SEQUENCE IF NOT EXISTS venues_seq INCREMENT BY 1; \
-    SELECT setval(\'venues_seq\', SELECT max(venue_id) FROM venue) FROM venue;'
+    SELECT setval(\'venues_seq\', max(venue_id)) FROM venue;'
 
-    db_client.query(query, [request.body.venue_name, request.body.city_name, request.body.country_name, capacity], (err, res) => {
+    db_client.query(query, (err, res) => {
       if (err) {
         console.log(err)
         response.status(500).json({ error: err.message })
