7edea187c57a47b0eed4b0af05c6f18ea3c76df7
vibhav011 <vibhav200601@gmail.com>
2022-02-06 01:27:12 +0530

    Fixed part D query

--- backend/pointstable/get_pointstable.js
+++ backend/pointstable/get_pointstable.js
@@ -6,13 +6,23 @@ function get_pointstable(db_client, request, response) { // request and response
     }
 
     var ret_json = null
-    const query = 'select team_id, team_name, mat, won, mat-won as lost, \'0\' as tied, nr, 2*won as pts \
-    from (select team_id, team_name, count(distinct match_id) mat, sum(case when match_winner=team_id then 1 else 0 end)/2 won, \
-    round(sum(sign*(2*innings_no-3)*nr), 2) nr from(select team_id, team_name, match.match_id, match_winner, innings_no, \
-    case when (toss_winner=team_id and toss_name=\'bat\') or (toss_winner!=team_id and toss_name=\'field\') then -1 else 1 end sign, \
-    sum(runs_scored+extra_runs)*1.0/count(distinct over_id) nr from team, match, ball_by_ball as bb where bb.match_id=match.match_id \
-    and (team1=team_id or team2=team_id) and season_year=$1 group by team_id, team_name, match.match_id, match_winner, innings_no, \
-    toss_winner, toss_name) t group by team_id, team_name) t2 order by won desc;'
+    const query = `with mr as (select match_id, innings_no, sum(runs_scored+extra_runs) runs, count(distinct over_id) overs
+                from ball_by_ball where match_id in (select match_id from match where season_year=$1) group by match_id, innings_no),
+                match_runs as (select mr1.match_id, mr1.runs as inn1_runs, mr2.runs as inn2_runs, mr1.overs as overs1, mr2.overs as overs2
+                from mr as mr1, mr as mr2 where mr1.match_id=mr2.match_id and mr1.innings_no=1 and mr2.innings_no=2),
+                team_runs as (select mr.match_id, team1, team2, inn1_runs, inn2_runs, overs1, overs2,
+                case when (toss_winner=team1 and toss_name='bat') or (toss_winner=team2 and toss_name='field') then 1 else 2 end turn,
+                case when match_winner=team1 then 1 else 0 end win from match_runs as mr, match where match.match_id = mr.match_id),
+    
+                team_info as ((select team1 as team_id, match_id, case when turn=1 then inn1_runs else inn2_runs end runs,
+                case when turn=1 then overs1 else overs2 end overs, inn1_runs+inn2_runs as both_runs, overs1+overs2 as both_overs, win from team_runs)
+                union (select team2 as team_id, match_id, case when turn=2 then inn1_runs else inn2_runs end runs,
+                case when turn=2 then overs1 else overs2 end overs, inn1_runs+inn2_runs as both_runs, overs1+overs2 as both_overs, 1-win as win from team_runs)),
+    
+                team_nr as (select team_id, round(sum(runs)/sum(overs)-sum(both_runs-runs)/sum(both_overs-overs), 2) nr, sum(win) as won,
+                count(match_id) as mat from team_info group by team_id)
+                select team.team_id, team_name, mat, won, mat-won as lost, 0 as tied, nr, 2*won as pts 
+                from team_nr, team where team.team_id = team_nr.team_id and mat > 0 order by won desc, nr desc;`
 
     db_client.query(query, [year], (err, res) => {
         if (err) {
