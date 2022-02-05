1edbf2cbdfa865b8903539003eeb57425fce0cbd
Tulip16 <tulip@cse.iitb.ac.in>
2022-02-06 02:36:12 +0530

    score chased updated

--- backend/venues/get_venue_basic_info.js
+++ backend/venues/get_venue_basic_info.js
@@ -7,16 +7,16 @@ function get_venue_basic_info(db_client, request, response) { // request and res
       return
     }
 
-    const query = 'select t1.venue_name, t1.city_name, t1.country_name, t1.capacity,  highest_total_recorded, lowest_total_recorded, highest_score_chased, chased from \
+    const query = 'select t1.venue_name, t1.city_name, t1.country_name, t1.capacity,  highest_total_recorded, lowest_total_recorded, highest_score_chased from \
     (select venue_name, city_name, country_name, capacity, count(distinct match_id) as total_matches_played, max(innings_runs) as highest_total_recorded, \
    min(innings_runs) as lowest_total_recorded from (select match.match_id, innings_no, sum(runs_scored+extra_runs) as innings_runs from match, \
    ball_by_ball where ball_by_ball.match_id = match.match_id and match.venue_id = $1 group by match.match_id, innings_no) sq, venue where venue.venue_id = $1 \
    group by venue_id) t1, \
-   (select venue_name, city_name, country_name, capacity, max(innings1_runs) as highest_score_chased from (select match.match_id, sum(runs_scored+extra_runs) as innings1_runs from match, \
+   (select venue_name, city_name, country_name, capacity, max(innings1_runs*sq.chased) as highest_score_chased from (select match.match_id, sum(runs_scored+extra_runs) as innings1_runs, \
+   case when exists(select match_id from match as m2 where (((m2.match_winner = m2.toss_winner and m2.toss_name = \'field\') \
+   or (m2.match_winner != m2.toss_winner and m2.toss_name = \'bat\')) and m2.match_id = match.match_id)) then 1 else 0 end chased from match, \
    ball_by_ball where ball_by_ball.match_id = match.match_id and match.venue_id = $1 and innings_no = 1 group by match.match_id) sq, venue where venue.venue_id = $1 \
-   group by venue_id) t2,\
-   (select exists(select match_id from match where ((match_winner = toss_winner and toss_name = \'field\') \
-   or (match_winner != toss_winner and toss_name = \'bat\')) and venue_id = $1) as chased) t3'
+   group by venue_id) t2'
 
     db_client.query(query, [venue_id], (err, res) => {
       if (err) {
